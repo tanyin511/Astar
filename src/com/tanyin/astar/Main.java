9f9b6366a75aa66db12e2464d772afc884af1b2c
tanyin511 <yin.tan@qq.com>
2017-02-03 21:54:21 +0800

    main 运行

--- src/com/tanyin/astar/Main.java
+++ src/com/tanyin/astar/Main.java
@@ -4,4 +4,7 @@ package com.tanyin.astar;
  * Created by tanyin on 2017/2/3.
  */
 public class Main {
+    public static void main(String[] args) {
+        System.out.println("hello test is run~~");
+    }
 }
